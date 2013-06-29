3865773f580738c549e59c5f6a13da09e0e3d51c
İ. Emre Kutlu <emrekutlu@gmail.com>
2013-06-29 13:39:33 +0300

    download counts as png

--- main.go
+++ main.go
@@ -5,8 +5,17 @@ import (
     "fmt"
     "appengine"
     "appengine/urlfetch"
+//    "os"
+    "log"
+    "image"
+    "image/draw"
+    "image/color"
+    "image/png"
+    "io/ioutil"
+    "strconv"
     "github.com/emrekutlu/go-rubygems"
     "github.com/gorilla/mux"
+    "code.google.com/p/freetype-go/freetype"
 )
 
 func init() {
@@ -15,7 +24,7 @@ func init() {
     r.HandleFunc("/", root)
     gemsRouter := r.PathPrefix("/gems/{gem}").Subrouter()
     gemsRouter.HandleFunc("/", gems)
-    gemsRouter.HandleFunc("/downloads", downloads)
+    gemsRouter.HandleFunc("/downloads.png", downloads)
 
     http.Handle("/", r)
 }
@@ -40,17 +49,72 @@ func gems(w http.ResponseWriter, r *http.Request) {
 }
 
 func downloads(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-type", "image/png")
     c := appengine.NewContext(r)
     client := urlfetch.Client(c)
     vars := mux.Vars(r)
 
     rubygems.Initialize(client)
     gem, err := rubygems.NewGem(vars["gem"]).Get()
+/*
+    file, err := os.Open("static/badge.png")
+    if err != nil {
+      log.Fatal(err)
+    } else {
+      defer file.Close()
+    }
+
+    // Decode the image.
+    m, _, err := image.Decode(file)
+   if err != nil {
+     log.Fatal(err)
+   }
+*/
+    m := image.NewRGBA(image.Rect(0, 0, 100, 20))
+    blue := color.RGBA{0, 29, 204, 170}
+    draw.Draw(m, m.Bounds(), &image.Uniform{C: blue}, image.ZP, draw.Src)
+
+    // Freetype
+
+// Read the font data.
+   fontBytes, err := ioutil.ReadFile("/Users/emrekutlu/www/code-badges/Signika-Regular.ttf")
+   if err != nil {
+     log.Println(err)
+     return
+   }
+   font, err := freetype.ParseFont(fontBytes)
+   if err != nil {
+     log.Println(err)
+     return
+   }
+
+    var size float64
+    size = 12
+    freetypeContext := freetype.NewContext()
+    freetypeContext.SetDPI(72)
+    freetypeContext.SetFont(font)
+    freetypeContext.SetFontSize(size)
+    freetypeContext.SetClip(m.Bounds())
+    freetypeContext.SetDst(m)
+    freetypeContext.SetSrc(image.White)
+
+    pt := freetype.Pt(40, int(freetypeContext.PointToFix32(size)>>8))
+
+   for _, s := range []string{strconv.Itoa(gem.Downloads)} {
+     _, err = freetypeContext.DrawString(s, pt)
+     if err != nil {
+       log.Println(err)
+       return
+     }
+     pt.Y += freetypeContext.PointToFix32(size * 1.5)
+   }
+
 
     if err != nil {
       fmt.Fprint(w, err)
     } else {
-      fmt.Fprint(w, gem.Downloads)
+      log.Println(gem.Downloads)
+      png.Encode(w, m)
     }
 }
 
